dedfbf04a6dceec0d9012513bd060872425f5d91
abanet <alberto.banet@gmail.com>
2016-09-21 20:40:13 +0200

    Primera versión básica con un cronómetro

--- cronoTask/ViewController.swift
+++ cronoTask/ViewController.swift
@@ -9,9 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  var startTime = NSTimeInterval()
+  var timer = NSTimer()
 
+
+  
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    
     // Do any additional setup after loading the view, typically from a nib.
   }
 
@@ -19,7 +27,42 @@ class ViewController: UIViewController {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+  
+  @IBAction func iniciarCronometro(sender: AnyObject) {
+    
+    let aSelector : Selector = #selector(ViewController.updateTime)
+    timer = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: aSelector, userInfo: nil, repeats: true)
+    startTime = NSDate.timeIntervalSinceReferenceDate()
+    
+    
+  }
+  
+  @IBAction func pararCronometro(sender: AnyObject) {
+    timer.invalidate()
+  }
 
+  
+  func updateTime(){
+    let currentTime = NSDate.timeIntervalSinceReferenceDate()
+    var tiempoPasado: NSTimeInterval = currentTime - startTime
+    
+    
+    let minutos = UInt8(tiempoPasado/60.0)
+    tiempoPasado -= (NSTimeInterval(minutos)*60)
+    
+    let segundos = UInt8(tiempoPasado)
+    tiempoPasado -= NSTimeInterval(segundos)
+    
+    let fraccion = UInt8(tiempoPasado * 100)
+  
+    let strMinutos = String(format:"%02d",minutos)
+    let strSegundos = String(format:"%02d", segundos)
+    let strFracciones = String(format:"%02d",fraccion)
+    
+//    lblHoras.text = strMinutos
+//    lblMinutos.text = strSegundos
+//    lblSegundos.text = strFracciones
+  }
 
 }
 
